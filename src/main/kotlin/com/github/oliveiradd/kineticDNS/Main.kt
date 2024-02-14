af2b8f0c27f43965849ba6095daf0b77e40c2246
Daniel <oliveira.d.c@outlook.com>
2024-02-14 13:17:39 -0300

    remove ; needed in java

--- src/main/kotlin/com/github/oliveiradd/kineticDNS/Main.kt
+++ src/main/kotlin/com/github/oliveiradd/kineticDNS/Main.kt
@@ -1,6 +1,6 @@
 package com.github.oliveiradd.kineticDNS
 
-import java.io.IOException;
+import java.io.IOException
 
 // work with files and paths
 import java.nio.file.Files
@@ -9,7 +9,7 @@ import java.nio.file.Paths
 
 //check if file is writable
 // import java.nio.file.Filesystems
-import java.util.logging.Logger;
+import java.util.logging.Logger
 
 val projectName = "kineticDNS"
 val logger = LogManager(projectName,true)
@@ -17,100 +17,100 @@ val ipMonitor = LogManager("ipMonitor",true)
 
 fun main(args: Array<String>) {
     // Check configuration file
-    val configFolderNames: Array<String> = arrayOf("/usr/local/etc","/etc",".");
-    val configFileName: String = projectName + ".conf";
+    val configFolderNames: Array<String> = arrayOf("/usr/local/etc","/etc",".")
+    val configFileName: String = projectName + ".conf"
 
-    val configFolderPaths = Array<Path?>(configFolderNames.size) { null };
+    val configFolderPaths = Array<Path?>(configFolderNames.size) { null }
     for (i in configFolderNames.indices) {
-        configFolderPaths[i] = Paths.get(configFolderNames[i]);
+        configFolderPaths[i] = Paths.get(configFolderNames[i])
     }
 
-    var configFolderName: String = "";
+    var configFolderName: String = ""
     for (i in configFolderNames.indices) {
         if (Files.exists(configFolderPaths[i]) && Files.isDirectory(configFolderPaths[i])) {
-            configFolderName = configFolderNames[i];
-            break;
+            configFolderName = configFolderNames[i]
+            break
         }
     }
 
-    val configFile: String = configFolderName + "/" + configFileName;
-    val configFilePath: Path = Paths.get(configFile);
+    val configFile: String = configFolderName + "/" + configFileName
+    val configFilePath: Path = Paths.get(configFile)
     
-    val configHandler: ConfigHandler = ConfigHandler(configFile);
+    val configHandler: ConfigHandler = ConfigHandler(configFile)
     
-    var operation: String = "check-update";
+    var operation: String = "check-update"
 
     for (i in args.indices) {
         when (args[i]) {
             "-f","--force" -> operation = "force-up"
-            "-c","--configure" -> operation = "configure";
+            "-c","--configure" -> operation = "configure"
         }
     }
     
     if (!Files.exists(configFilePath) || operation.equals("configure")) {
-        if (!Files.exists(configFilePath)) System.err.println("Configuration file not found in "+configFile);
+        if (!Files.exists(configFilePath)) System.err.println("Configuration file not found in "+configFile)
         if (!hasWritePrivileges(configFolderName)){
-            System.err.println("No write privileges to "+configFolderName);
-            System.err.println("Try running the program as root to write the configuration file.");
-            System.exit(0);
+            System.err.println("No write privileges to "+configFolderName)
+            System.err.println("Try running the program as root to write the configuration file.")
+            System.exit(0)
         } else {
-            System.out.println("Configuring "+projectName+"...");
-            configHandler.createConfig();
-            return;
+            System.out.println("Configuring "+projectName+"...")
+            configHandler.createConfig()
+            return
         }
     }
-    configHandler.readConfig();
-    val DDNS_provider: String = configHandler.getProperty("DDNS_provider");
-    val IPLookup_service: String = configHandler.getProperty("IPLookup_service");
-    //String currentIP = kineticDNS.getCurrentIP();
+    configHandler.readConfig()
+    val DDNS_provider: String = configHandler.getProperty("DDNS_provider")
+    val IPLookup_service: String = configHandler.getProperty("IPLookup_service")
+    //String currentIP = kineticDNS.getCurrentIP()
     
     if (DDNS_provider.equals(ConfigHandler.DDNSProviders[0])) {
-        val username: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[0]);
-        val password: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[1]);
-        val hostname: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[2]);
-        val domain: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[3]);
+        val username: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[0])
+        val password: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[1])
+        val hostname: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[2])
+        val domain: String = configHandler.getProperty(ConfigHandler.NoIPConfigurationParameters[3])
         //procedes to update DDNS
-        //System.out.println(username+" "+password+" "+hostname+" "+domain);
+        //System.out.println(username+" "+password+" "+hostname+" "+domain)
         try {
-            val currentIP: String = kineticDNS.getCurrentIP(IPLookup_service);
+            val currentIP: String = kineticDNS.getCurrentIP(IPLookup_service)
             if (!(ipMonitor.getLastLine().contains(currentIP)) || operation.equals("force-update")) {
-                val response: String = kineticDNS.NoIpUpdate(username, password, hostname, domain);
-                if (kineticDNS.isResponseAppropriate(response)) ipMonitor.info("Retrieved IP: "+currentIP);
+                val response: String = kineticDNS.NoIpUpdate(username, password, hostname, domain)
+                if (kineticDNS.isResponseAppropriate(response)) ipMonitor.info("Retrieved IP: "+currentIP)
             }
         } catch (e: IOException) {
-            logger.warning("Failed to get current IP address.");
+            logger.warning("Failed to get current IP address.")
         }
     }
 
     if (DDNS_provider.equals(ConfigHandler.DDNSProviders[1])) {
-        val username: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[0]);
-        val password: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[1]);
-        val hostname: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[2]);
-        val domain: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[3]);
+        val username: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[0])
+        val password: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[1])
+        val hostname: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[2])
+        val domain: String = configHandler.getProperty(ConfigHandler.DynDNSConfigurationParameters[3])
         //procedes to update DDNS
-        //System.out.println(username+" "+password+" "+hostname+" "+domain);
+        //System.out.println(username+" "+password+" "+hostname+" "+domain)
         try {
-            val currentIP: String = kineticDNS.getCurrentIP(IPLookup_service);
+            val currentIP: String = kineticDNS.getCurrentIP(IPLookup_service)
             if (!(ipMonitor.getLastLine().contains(currentIP)) || operation.equals("--force")) {
-                val response: String = kineticDNS.DynDNSUpdate(username, password, hostname, domain,currentIP);
-                if (kineticDNS.isResponseAppropriate(response)) ipMonitor.info("Retrieved IP: "+currentIP);
+                val response: String = kineticDNS.DynDNSUpdate(username, password, hostname, domain,currentIP)
+                if (kineticDNS.isResponseAppropriate(response)) ipMonitor.info("Retrieved IP: "+currentIP)
             }
         } catch (e: IOException) {
-            logger.warning("Failed to get current IP address.");
+            logger.warning("Failed to get current IP address.")
         }
     }
 
     if (DDNS_provider.equals(ConfigHandler.DDNSProviders[2])) {
-        val token: String = configHandler.getProperty(ConfigHandler.DuckDNSConfigurationParameters[0]);
-        val hostname: String = configHandler.getProperty(ConfigHandler.DuckDNSConfigurationParameters[1]);
+        val token: String = configHandler.getProperty(ConfigHandler.DuckDNSConfigurationParameters[0])
+        val hostname: String = configHandler.getProperty(ConfigHandler.DuckDNSConfigurationParameters[1])
         try {
-            val currentIP: String = kineticDNS.getCurrentIP(IPLookup_service);
+            val currentIP: String = kineticDNS.getCurrentIP(IPLookup_service)
             if (!(ipMonitor.getLastLine().contains(currentIP)) || operation.equals("--force")) {
-                val response: String = kineticDNS.DuckDNSUpdate(hostname, token);
-                if (kineticDNS.isResponseAppropriate(response)) ipMonitor.info("Retrieved IP: "+currentIP);
+                val response: String = kineticDNS.DuckDNSUpdate(hostname, token)
+                if (kineticDNS.isResponseAppropriate(response)) ipMonitor.info("Retrieved IP: "+currentIP)
             }
         } catch (e: IOException) {
-            logger.warning("Failed to get current IP address.");
+            logger.warning("Failed to get current IP address.")
         }
     }        
 }
